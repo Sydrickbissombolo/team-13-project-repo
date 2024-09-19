9420bbce52850ddb4c5c6869a4b9fb77e2dd183e
KukkikD <adeezon9@gmail.com>
2024-09-19 00:29:58 -0700

    ah added cart activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve the current cart items from localStorage
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  // Add the product to the list
+  cartItems.push(product);
+
+  // Save the updated cart list back to localStorage
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
